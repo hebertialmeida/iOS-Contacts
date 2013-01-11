16561b3080b391c3eda74d2b7eb35a6d3ea316f5
Heberti Almeida <hebertialmeida@gmail.com>
2013-01-11 09:26:04 -0200

    Initial Commit

---
+++ Contatos/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Contatos
+//
+//  Created by Heberti Almeida on 11/01/13.
+//  Copyright (c) 2013 Heberti Almeida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
